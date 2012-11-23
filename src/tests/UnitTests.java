aecad232e1887e781e21dcafebfa27426b8baf81
Haukur Rosinkranz <haukurr11@ru.is>
2012-11-23 16:39:37 +0000

    Junit einingarprófunartólinu bætt við

---
+++ src/tests/UnitTests.java
@@ -0,0 +1,11 @@
+package tests;
+
+import junit.framework.TestCase;
+
+public class UnitTests extends TestCase
+{
+    public void testStartGame()
+    {
+        assertEquals("expected","expected");
+    }
+}
